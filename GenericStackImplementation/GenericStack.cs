0e3fa788ebab7e488f7c6a14e45ed46df28b9030
Chilishikawa <chilishikawa13@gmail.com>
2025-09-09 16:25:58 -0300

    first charge

---
+++ GenericStackImplementation/GenericStack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericStackImplementation
+{
+    public class GenericStack<T>
+    {
+        private List<T> items = new List<T>();
+
+        public void Push(T item)
+        {
+            items.Add(item);
+        }
+
+        public T Pop()
+        {
+            if (items == null)
+            {
+                throw new InvalidOperationException("The collection is not initialized.");
+            }
+            
+            if (items.Count == 0)
+            {
+                throw new InvalidOperationException("Cannot pop from an empty collection.");
+            }
+
+            T poppedItem = items[items.Count - 1];
+            items.RemoveAt(items.Count - 1);
+            return poppedItem;
+        }
+
+        public List<T> Display()
+        {
+            return new List<T>(items);
+        }
+    }
+}
---
+++ GenericStackImplementation/Program.cs
@@ -0,0 +1,77 @@
+﻿using InteractiveLINQconsoleApp01;
+
+namespace GenericStackImplementation
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // INTEGERS
+            GenericStack<int> intCaseGeneric = new GenericStack<int>();
+
+            // Push 3 integers to list
+            intCaseGeneric.Push(1);
+            intCaseGeneric.Push(2);
+            intCaseGeneric.Push(3);
+
+            // Display all elements
+            var intList = intCaseGeneric.Display();
+            Console.WriteLine("List of integer added: " + string.Join(", ", intList));
+
+            // Remove last element and display remaining elements
+            intCaseGeneric.Pop();
+            var intsRemaining = intCaseGeneric.Display();
+            Console.WriteLine("List of integer remaining in list: " + string.Join(", ", intsRemaining));
+
+            //-------------
+            // INTEGERS
+            GenericStack<string> stringCaseGeneric = new GenericStack<string>();
+
+            // Push 3 strings to list
+            stringCaseGeneric.Push("uno");
+            stringCaseGeneric.Push("dos");
+            stringCaseGeneric.Push("tres");
+
+            // BLANK SPACE
+            Console.WriteLine("");
+
+            // Display all elements
+            var stringList = stringCaseGeneric.Display();
+            Console.WriteLine("List of strings added: " + string.Join(", ", stringList));
+
+            // Remove last element and display remaining elements
+            stringCaseGeneric.Pop();
+            var stringRemaining = stringCaseGeneric.Display();
+            Console.WriteLine("List of strings remaining in list: " + string.Join(", ", stringRemaining));
+
+            //-------------
+            // PERSON
+            GenericStack<Person> personCaseGeneric = new GenericStack<Person>();
+
+            // Push 3 strings to list
+            personCaseGeneric.Push(new Person ("UNO",11,111000));
+            personCaseGeneric.Push(new Person("DOS", 22, 222000));
+            personCaseGeneric.Push(new Person("UNO", 33, 333000));
+
+            // BLANK SPACE
+            Console.WriteLine("");
+
+            // Display all elements
+            var personList = personCaseGeneric.Display();
+            Console.WriteLine("List of people added to list:");
+            foreach (Person p in personList)
+            {
+                Console.WriteLine($"Name: {p.Name}, Age: {p.Age}, Salary: {p.Salary}");
+            }
+
+            // Remove last element and display remaining elements
+            personCaseGeneric.Pop();
+            var personRemaining = personCaseGeneric.Display();
+            Console.WriteLine("List of people remaining in list:");
+            foreach (Person p in personRemaining)
+            {
+                Console.WriteLine($"Name: {p.Name}, Age: {p.Age}, Salary: {p.Salary}");
+            }
+        }
+    }
+}
